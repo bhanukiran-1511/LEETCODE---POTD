75571418cc70bee1597bbdf373d82a2bb44f3742
bhanukiran-1511 <bhanukiranpilla@gmail.com>
2025-07-18 20:54:10 +0530

    18-07-25

--- 1353. Maximum Number of Events That Can Be Attended.cpp
+++ 1353. Maximum Number of Events That Can Be Attended.cpp
@@ -1,31 +1,52 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <functional>
-using namespace std;
-
 class Solution {
 public:
-    int maxEvents(vector<vector<int>>& events) {
-        int n = events.size();
-        sort(events.begin(), events.end());
-        int day = events[0][0];
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int cnt = 0;
-        int i = 0;
-        while (!pq.empty() || i < n) {
-            while (i < n && events[i][0] == day) {
-                pq.push(events[i][1]);
-                i++;
+    long long minimumDifference(vector<int>& nums) {
+        int N = nums.size();  // Total number of elements, must be 3n
+        int n = N / 3;        // n-size for each group
+
+        // Arrays to store the minimum left sum and maximum right sum at each index
+        vector<long long> leftmin(N, 0);
+        vector<long long> rightmax(N, 0);
+
+        // Max-heap to maintain the n smallest elements sum on the left
+        priority_queue<int> maxheap;
+        long long leftsum = 0;
+
+        // Process the first 2n elements to find the smallest possible sum by picking n elements
+        for (int i = 0; i < 2 * n; i++) {
+            leftsum += nums[i];
+            maxheap.push(nums[i]);
+            // Keep only the n smallest elements in maxheap
+            if (maxheap.size() > n) {
+                leftsum -= maxheap.top(); // Remove largest to keep n elements
+                maxheap.pop();
             }
-            if (!pq.empty()) {
-                pq.pop();
-                cnt++;
-            }
-            day++;
-            while (!pq.empty() && pq.top() < day)
-                pq.pop();
+            leftmin[i] = leftsum; // Store the sum after keeping n smallest so far
         }
-        return cnt;
+
+        // Min-heap to maintain the n largest elements sum on the right
+        priority_queue<int, vector<int>, greater<int>> minheap;
+        long long rightsum = 0;
+
+        // Process the last 2n elements in reverse to find the largest possible sum by picking n elements
+        for (int i = N - 1; i >= n; i--) {
+            rightsum += nums[i];
+            minheap.push(nums[i]);
+            // Keep only the n largest elements in minheap
+            if (minheap.size() > n) {
+                rightsum -= minheap.top(); // Remove smallest to keep n elements
+                minheap.pop();
+            }
+            rightmax[i] = rightsum; // Store the sum after keeping n largest so far
+        }
+
+        // Find the minimum difference between leftmin and rightmax
+        long long minans = LLONG_MAX;
+        // Consider boundaries between left and right partitions
+        for (int i = n - 1; i < 2 * n; i++) {
+            minans = min(minans, leftmin[i] - rightmax[i + 1]);
+        }
+
+        return minans; // Return the smallest difference found
     }
 };
