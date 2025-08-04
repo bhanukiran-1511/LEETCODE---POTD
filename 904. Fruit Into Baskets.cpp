aa51b54e9ca07dd8e29b62e6d06380ed13a1377b
bhanukiran-1511 <bhanukiranpilla@gmail.com>
2025-08-04 19:16:05 +0530

    04-08-25

---
+++ 904. Fruit Into Baskets.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n=fruits.size();
+        unordered_map<int,int>mpp;
+        int l=0;
+        int maxi=0;
+        for(int r=0;r<n;r++){
+            mpp[fruits[r]]++;
+            while(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+                l++;
+            }
+            maxi=max(maxi,r-l+1);
+        }
+        return maxi;
+    }
+};
